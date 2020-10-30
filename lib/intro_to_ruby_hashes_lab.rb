3e57bada5c17f0ae26a4d5208c58a4bdc85d7883
Sin Wildy <sw.wildy@gmail.com>
2020-10-30 00:22:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,11 @@ def my_hash
 end
 
 def pioneer
-pioneer[:name]
+pioneer[:name] = "Grace Hopper"
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-end
+end 
